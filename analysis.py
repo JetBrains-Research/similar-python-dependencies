e2477039438457a914167df1d20764693d2df052
areyde <areyde.elvgren@gmail.com>
2020-12-08 13:24:36 +0300

    The initial upload of scripts and data

---
+++ analysis.py
@@ -0,0 +1,397 @@
+# Processing raw data, building and training models, suggesting closest projects.
+
+from collections import Counter
+from math import log
+from operator import itemgetter
+import os
+from typing import Dict, List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import requirements
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.manifold import TSNE
+from tqdm import tqdm
+
+
+def process_sources() -> None:
+    """
+    Parses through all the versious of requirements files, saves the processed version
+    into a single file.
+    :return: None
+    """
+    dataset = []
+    # Get the list of all the projects.
+    with open("sources/requirements_history/download.txt") as fin:
+        for line in fin:
+            data = line.rstrip().split("/")
+            dataset.append((data[0], data[1]))
+            dataset = sorted(dataset, key=itemgetter(0, 1))
+    # Iterate over all the versions from the old towards the new.
+    with open("processed/requirements_history.txt", "w+") as fout:
+        for date in ["2011-11-22", "2012-11-21", "2013-11-21", "2014-11-21", "2015-11-21",
+                     "2016-11-20", "2017-11-20", "2018-11-20", "2019-11-20", "2020-11-19"]:
+            # Iterate over all the projects.
+            for repo in dataset:
+                path = f"sources/requirements_history/{date}/{repo[0]}_{repo[1]}.txt"
+                if os.path.exists(path):
+                    with open(path) as fin_req:
+                        try:
+                            reqs_list = []
+                            # Parse the `requirements.txt` and save all the dependencies
+                            # in the form "reqirement:1" (remained from topic modelling)
+                            reqs = requirements.parse(fin_req)
+                            for req in reqs:
+                                reqs_list.append(req.name)
+                            if len(reqs_list) != 0:
+                                fout.write(
+                                    f"{repo[0]}_{repo[1]}/{date};{','.join([req + ':1' for req in reqs_list])}\n")
+                        except:
+                            continue
+
+
+def read_dependencies(file: str) -> Dict[str, List[str]]:
+    """
+    Read the file with the dependencies and return a dictionary with repos as keys
+    and lists of their dependencies as values. Automatically considers the lower- and upppercase,
+    and exchanges "-" to "_".
+    :param file: name of the file, not the full path.
+    :return: dictionary {repo: [dependencies], ...}
+    """
+    reqs = {}
+    with open(f"processed/{file}") as fin:
+        for line in fin:
+            data = line.rstrip().split(";")
+            reqs[data[0]] = [req.split(":")[0].lower().replace("-", "_") for req in data[1].split(",")]
+    return reqs
+
+
+def train_svd(file: str) -> None:
+    """
+    Create a project-dependencies matrix and train an SVD (singular vector decomposition)
+    that will transform the lines into 32-metric embeddings.
+    :param file: name of the file, not the full path.
+    :return: None.
+    """
+    # Get the dictionary from projects to dependencies.
+    reqs = read_dependencies(file)
+
+    # Get the list of all the dependencies.
+    dependency_list = []
+    for repo in reqs:
+        for req in reqs[repo]:
+            dependency_list.append(req)
+
+    repos_list = list(reqs.keys()) # List of the projects, ordered by versions
+    dependency_counter = sorted(Counter(dependency_list).items(), key=itemgetter(1), reverse=True)
+    dependency_list = sorted(list(set(dependency_list))) # List of the dependencies
+
+    print(f"There are a total of {len(repos_list)} repositories "
+          f"and {len(dependency_list)} dependencies.")
+
+    # Create an empty matrix of necessary size.
+    matrix = np.zeros((len(repos_list), len(dependency_list)))
+    print(f"The shape of the matrix for SVD is {matrix.shape}.")
+
+    # Fill the matrix with 1's
+    for index_repo, repo in enumerate(repos_list):
+        for req in reqs[repo]:
+            matrix[index_repo, dependency_list.index(req)] = 1
+
+    # Train an SVD, transform the matrix, and save it.
+    svd = TruncatedSVD(n_components=32, n_iter=7)
+    new_matrix = svd.fit_transform(matrix)
+    np.save(f"models/{file}", new_matrix)
+    # Save the list of projects in correct order.
+    with open(f"models/{file}_repos", "w+") as fout:
+        for repo in repos_list:
+            fout.write(f"{repo}\n")
+    # Save the IDFs of the dependencies.
+    with open(f"models/{file}_dependencies", "w+") as fout:
+        for dependency in dependency_counter:
+            fout.write(f"{dependency[0]};"
+                       f"{round(log(len(repos_list)/dependency[1]), 3)}\n")
+    print(f"SVD trained and saved. The new shape of the matrix is {new_matrix.shape}.")
+
+
+def predict_closest(file: str, names: List[str], amount: int, single_version: bool,
+                    filter_versions: bool) -> Dict[str, List[Tuple[str, str, float]]]:
+    """
+    Given the list of names of projects, find the closest to them in the embedding space,
+    and return them.
+    :param file: name of the file, not the full path.
+    :param names: a list of full repo names that must be searched.
+    :param amount: number of the closest repos to find for each query project.
+    :param single_version: if True, will only consider the repos of the same version as query.
+    :param filter_versions: if True, only the closest version of any repo will be in the output.
+    :return: dictionary {repo: [(close_repo, version, similarity), ...]}.
+    """
+    data = np.load(f"models/{file}.npy")
+    repos_list = []
+    with open(f"models/{file}_repos") as fin:
+        for line in fin:
+            repos_list.append(line.rstrip())
+    closest = {}
+    print("Searching for the closest repos...")
+    # Iterating over query projects.
+    for name in tqdm(names):
+        target_vector = data[repos_list.index(name)]
+        cosines = []
+        # Iterating over all the repos.
+        for index_repo, vector in enumerate(data):
+            if repos_list[index_repo].split("/")[0] == name.split("/")[0]:
+                continue # Skip the query repo itself.
+            if single_version is True:
+                if repos_list[index_repo].split("/")[1] != name.split("/")[1]:
+                    continue # Skip other versions.
+            # Calculate and store the similarity.
+            similarity = cosine_similarity([target_vector], [vector])
+            cosines.append([repos_list[index_repo].split("/")[0],
+                            repos_list[index_repo].split("/")[1],
+                            similarity[0][0]])
+        max_cosines = sorted(cosines, key=itemgetter(2), reverse=True)
+        # Since we don't know, which version of each repo is the closest,
+        # this filtering is done post-factum.
+        if (single_version is False) and (filter_versions is True):
+            processed_repos = set()
+            filtered_max_cosines = []
+            for repo in max_cosines:
+                # Skip the query repo itself.
+                if repo[0] == name.split("/")[0]:
+                    continue
+                else:
+                    if repo[0] not in processed_repos:
+                        # Only save the closest version and skip all the rest.
+                        filtered_max_cosines.append(repo)
+                        processed_repos.add(repo[0])
+            max_cosines = filtered_max_cosines
+        closest[name] = max_cosines[:amount]
+    return closest
+
+
+def print_closest(file: str, name: str, amount: int,
+                  single_version: bool, filter_versions: bool = True) -> None:
+    """
+    Run the `predict_closest` function and print the results.
+    :param file: name of the file, not the full path.
+    :param names: a list of full repo names that must be searched.
+    :param amount: number of the closest repos to find for each query project.
+    :param single_version: if True, will only consider the repos of the same version as query.
+    :param filter_versions: if True, only the closest version of any repo will be in the output.
+    :return: None.
+    """
+    closest = predict_closest(file, [name], amount, single_version, filter_versions)[name]
+    for repo in closest:
+        print(repo[0], repo[1], repo[2])
+
+
+def suggest_libraries(file: str, names: List[str], single_version: bool,
+                      idf_power: float) -> Dict[str, List[Tuple[str, float]]]:
+    """
+    Given a list of query projects, suggests potential libraries for all of them.
+    :param file: name of the file, not the full path.
+    :param names: a list of full repo names that must be searched.
+    :param single_version: if True, will only consider the repos of the same version as query.
+    :param idf_power: the power of IDF in the recommendation formula.
+    :return: dictionary {repo: [(suggestion, count), ...]}.
+    """
+    # Upload the dependencies and their IDFs.
+    reqs = read_dependencies(file)
+    idfs = {}
+    with open(f"models/{file}_dependencies") as fin:
+        for line in fin:
+            data = line.rstrip().split(";")
+            idfs[data[0]] = float(data[1])
+    suggestions = {}
+    # Find the closest repos for all the repos (faster to do in bulk).
+    closest = predict_closest(file, names, 20, single_version, True)
+    print("Analyzing libraries of the closest repos...")
+    for name in tqdm(names): # Iterate over query repos.
+        libraries = {}
+        for repo in closest[name]: # Iterate over the closest repos.
+            for req in reqs[repo[0] + "/" + repo[1]]: # Iterate over dependencies.
+                if req not in reqs[name]: # Skip if the given requirement already in the query.
+                    if req not in libraries:
+                        # Cosine distance to repo * IDF of lib
+                        libraries[req] = repo[2] * (idfs[req] ** idf_power)
+                    else:
+                        libraries[req] += repo[2] * (idfs[req] ** idf_power)
+        # Sort the suggestions by their score.
+        suggestions[name] = sorted(libraries.items(), key=itemgetter(1), reverse=True)
+    return suggestions
+
+
+def print_libraries(file: str, name: str, single_version: bool,
+                    idf_power: float, n_suggest: int) -> None:
+    """
+    Run the `suggest_libraries` function and print the result.
+    :param file: name of the file, not the full path.
+    :param names: a list of full repo names that must be searched.
+    :param single_version: if True, will only consider the repos of the same version as query.
+    :param idf_power: the power of IDF in the recommendation formula.
+    :param n_suggest: number of top libraries to suggest.
+    :return: None.
+    """
+    reqs = read_dependencies(file)
+    suggestions = suggest_libraries(file, [name], single_version, idf_power)[name][:n_suggest]
+    print(f"Repo name: {name}\n"
+          f"Repo dependencies: {', '.join(reqs[name])}\n\n"
+          f"Suggestions:\n")
+    for suggestion in suggestions:
+        print(f"{suggestion[0]}, score: {suggestion[1]}")
+
+
+def cluster_vectors(file: str, algo: str) -> None:
+    """
+    Cluster the embeddings using a certain algorithm and save the obtained labels.
+    :param file: name of the file, not the full path.
+    :param algo: "dbscan" or "kmeans".
+    :return: None.
+    """
+    data = np.load(f"models/{file}.npy")
+    # Cluster the embeddings using the given algorithm.
+    if algo == "dbscan":
+        clustering = DBSCAN(eps=0.15, min_samples=3, metric='cosine').fit(data)
+        labels = clustering.labels_
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+        print(f"Clusters: {n_clusters_}, noise: {n_noise_}.")
+    else:
+        n_clusters = 64
+        labels = KMeans(n_clusters=n_clusters).fit_predict(data)
+    # Run t-SNE to downscale the data into two dimensions.
+    data_downscaled = TSNE(n_components=2).fit_transform(data)
+    print(f"The shape of TSNE matrix is {data_downscaled.shape}.")
+    with open(f"models/{file}_clusters", "w+") as fout:
+        for index_repo, label in enumerate(labels):
+            # Save the label from the original clustering and 2D coordinates from t-SNE.
+            fout.write(f"{label};{data_downscaled[index_repo][0]};"
+                       f"{data_downscaled[index_repo][1]}\n")
+
+
+def visualize_clusters(file: str, mode: str) -> None:
+    """
+    Visualize the obtained clusterings either by clusters or by versions.
+    :param file: name of the file, not the full path.
+    :param mode: "clusters" for differentiate clusters by color, "versions" for versions.
+    :return: None
+    """
+    matrix = []
+    labels = []
+    # Upload the saved labels and coordinates of clusters.
+    with open(f"models/{file}_clusters") as fin:
+        for line in fin:
+            data = line.rstrip().split(";")
+            matrix.append([float(data[1]), float(data[2])])
+            labels.append(int(data[0]))
+    matrix = np.asarray(matrix)
+    if mode == "clusters":
+        matrix = np.flip(matrix, 0)
+        labels = np.flip(labels, 0)
+        plt.scatter(matrix[:, 0], matrix[:, 1], c=labels, s=50)
+        plt.title(f"Number of clusters: {len(set(labels))}")
+    else:
+        matrix = np.flip(matrix, 0)
+        versions_dict = {}
+        versions = []
+        # Create an ordered version of different numbers to color different versions.
+        with open(f"models/{file}_repos") as fin:
+            for line in fin:
+                version = line.rstrip().split("/")[1]
+                if version not in versions_dict:
+                    versions_dict = {"2011-11-22": 0, "2012-11-21": 1, "2013-11-21": 2,
+                                     "2014-11-21": 3, "2015-11-21": 4, "2016-11-20": 5,
+                                     "2017-11-20": 6, "2018-11-20": 7, "2019-11-20": 8,
+                                     "2020-11-19": 9}
+                versions.append(versions_dict[version])
+        versions = np.flip(versions, 0)
+        plt.scatter(matrix[:, 0], matrix[:, 1], c=versions, s=50)
+        plt.title(f"Number of versions: {len(set(versions))}")
+    plt.show()
+
+
+def analyze_pilgrims(file: str, n_show: int) -> None:
+    """
+    Given a matrix, study the projects that shifted the most between versions.
+    :param file: name of the file, not the full path.
+    :param n_show: number of pilgrims to show.
+    :return: None.
+    """
+    data = np.load(f"models/{file}.npy")
+    repos = []
+    with open(f"models/{file}_repos") as fin:
+        for line in fin:
+            repos.append(line.rstrip())
+    # Transform a list of full names into a dictionary from repo to all of its versions.
+    repos_dict = {}
+    for repo in repos:
+        r_v = repo.split("/")
+        if r_v[0] not in repos_dict:
+            repos_dict[r_v[0]] = [r_v[1]]
+        else:
+            repos_dict[r_v[0]].append(r_v[1])
+
+    adjacent_distances = []
+    poles_distances = []
+    for repo in repos_dict: # Iterate over repositories.
+        if len(repos_dict[repo]) != 1: # Skip repo if it only has 1 version (no dynamics).
+            # Iterate over all the adjacent versions.
+            for version in range(1, len(repos_dict[repo])):
+                vector_before = data[repos.index(f"{repo}/{repos_dict[repo][version - 1]}")]
+                vector_after = data[repos.index(f"{repo}/{repos_dict[repo][version]}")]
+                distance = 1 - cosine_similarity([vector_before], [vector_after])[0][0]
+                adjacent_distances.append([f"{repo}/{repos_dict[repo][version - 1]}",
+                                           f"{repo}/{repos_dict[repo][version]}",
+                                           distance])
+            # Additionally calculate the first and the last version.
+            vector_first = data[repos.index(f"{repo}/{repos_dict[repo][0]}")]
+            vector_last = data[repos.index(f"{repo}/{repos_dict[repo][-1]}")]
+            distance_poles = 1 - cosine_similarity([vector_first], [vector_last])[0][0]
+            poles_distances.append([f"{repo}/{repos_dict[repo][0]}",
+                                    f"{repo}/{repos_dict[repo][-1]}",
+                                    distance_poles])
+    # Sort all the pairs by the distance descending.
+    adjacent_distances = sorted(adjacent_distances, key=itemgetter(2), reverse=True)
+    poles_distances = sorted(poles_distances, key=itemgetter(2), reverse=True)
+    print("Adjacent distances:")
+    for i in range(n_show):
+        print(adjacent_distances[i][0], adjacent_distances[i][1], adjacent_distances[i][2])
+    print("Poles distances:")
+    for i in range(n_show):
+        print(poles_distances[i][0], poles_distances[i][1], poles_distances[i][2])
+
+
+def years_requirements(file: str) -> None:
+    """
+    Save the most popular requirement for each year separately.
+    :param file: name of the file, not the full path.
+    :return: None.
+    """
+    reqs = read_dependencies(file)
+    # Compile a dictionary {year: [depenencies]}
+    years = {}
+    for repo in reqs:
+        year = repo.split("/")[1]
+        if year not in years:
+            years[year] = []
+        years[year].extend(reqs[repo])
+    # Transform the lists into the Counters and pront them.
+    for year in years:
+        years[year] = [x[0] for x in sorted(Counter(years[year]).items(), key=itemgetter(1), reverse=True)]
+    with open(f"dynamics/{file}_years", "w+") as fout:
+        for year in years:
+            fout.write(f"{year};{','.join(years[year])}\n")
+
+
+if __name__ == "__main__":
+    # train_svd(file="requirements_history.txt")
+    # print_closest(file="requirements_history.txt", name="FeeiCN_EXIF/2019-11-20",
+    #               amount=10, single_version=True, filter_versions=True)
+    # print_libraries("requirements_history.txt", "AliShazly_sudoku-py/2020-11-19", True, 1, 10)
+    # cluster_vectors(file="requirements_history.txt", algo="kmeans")
+    # visualize_clusters(file="requirements_history.txt", mode="versions")
+    # analyze_pilgrims(file="requirements_history.txt", n_show=10)
+    # years_requirements(file="requirements_history.txt")
+    pass
---
+++ benchmark.py
@@ -0,0 +1,181 @@
+# Creating and using the automatic benchmark for suggestions.
+
+from statistics import mean
+from typing import Callable, Set, Tuple
+
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from tqdm import tqdm
+
+from analysis import read_dependencies, suggest_libraries
+
+
+def create_diffs(file: str) -> None:
+    """
+    Create a file with diffs, i.e. for each repo and adjacent versions list their requirements
+    in both versions.
+    :param file: name of the file, not the full path.
+    :return: None.
+    """
+    data = np.load(f"models/{file}.npy")
+    repos = []
+    with open(f"models/{file}_repos") as fin:
+        for line in fin:
+            repos.append(line.rstrip())
+    reqs = read_dependencies(file)
+    repos_dict = {}
+    # Get the dictionary {repo: [versions]}
+    for repo in repos:
+        r_v = repo.split("/")
+        if r_v[0] not in repos_dict:
+            repos_dict[r_v[0]] = [r_v[1]]
+        else:
+            repos_dict[r_v[0]].append(r_v[1])
+
+    with open(f"benchmark/{file}_diffs", "w+") as fout:
+        for repo in tqdm(repos_dict): # Iterate over repos.
+            if len(repos_dict[repo]) != 1:
+                for version in range(1, len(repos_dict[repo])):
+                    # Write both versions and their dependencies.
+                    vector_before = data[repos.index(f"{repo}/{repos_dict[repo][version - 1]}")]
+                    vector_after = data[repos.index(f"{repo}/{repos_dict[repo][version]}")]
+                    distance = 1 - cosine_similarity([vector_before], [vector_after])[0][0]
+                    fout.write(f"{repo};{repos_dict[repo][version - 1]};"
+                               f"{','.join(reqs[repo + '/' + repos_dict[repo][version - 1]])};"
+                               f"{repos_dict[repo][version]};"
+                               f"{','.join(reqs[repo + '/' + repos_dict[repo][version]])};"
+                               f"{distance}\n")
+
+
+def create_benchmark(file: str) -> None:
+    """
+    Create a benchmark: a list of projects and the libraries that they added
+    in the next version that must be guessed.
+    :param file: name of the file, not the full path.
+    :return: None.
+    """
+    with open(f"benchmark/{file}_diffs") as fin, open(f"benchmark/{file}_benchmark", "w+") as fout:
+        for line in tqdm(fin):
+            # Read the `diff` file.
+            data = line.rstrip().split(";")
+            repo, v_before, req_before, v_after, req_after =\
+                data[0], data[1], set(data[2].split(",")), data[3], set(data[4].split(","))
+            # If the new version has added at least 1 repo and changed less than 10, add it.
+            if (len(req_after.difference(req_before)) > 0) \
+                    and (len(req_after.difference(req_before)) -
+                         len(req_before.difference(req_after)) < 10):
+                fout.write(f"{repo}/{v_before};{','.join(req_after.difference(req_before))}\n")
+
+
+def baseline(file: str) -> None:
+    """
+    Create a baseline prediction file: for each repo, it predicts all the libraries in
+    that version, ordered by popularity.
+    :param file: name of the file, not the full path.
+    :return: None.
+    """
+    benchmark = []
+    with open(f"benchmark/{file}_benchmark") as fin: # Load the benchmark.
+        for line in fin:
+            benchmark.append(line.rstrip().split(";")[0])
+    reqs = {}
+    with open(f"dynamics/{file}_years") as fin: # Load the most popular libraries.
+        for line in fin:
+            data = line.rstrip().split(";")
+            reqs[data[0]] = data[1].split(",")
+    with open(f"benchmark/results/{file}-baseline-results", "w+") as fout: # Save the predictions.
+        for repo in benchmark:
+            fout.write(f"{repo};{','.join(reqs[repo.split('/')[1]])}\n")
+
+
+def run_benchmark(file: str, model: Callable) -> None:
+    """
+    Run the given model on a benchmark and save the prediction.
+    :param file: name of the file, not the full path.
+    :param model: the model that suggests libraries.
+    :return: None.
+    """
+    benchmark = []
+    with open(f"benchmark/{file}_benchmark") as fin: # Load the benchmark.
+        for line in fin:
+            benchmark.append(line.rstrip().split(";")[0])
+    benchmark = sorted(list(benchmark))
+    for idf_power in [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+        results = model(file, benchmark, True, idf_power) # Get the predictions.
+        with open(f"benchmark/results/{file}-{model.__name__}-{str(idf_power)}-results",
+                  "w+") as fout: # Save the results to file.
+            for repo in results:
+                fout.write(f"{repo};{','.join([x[0] for x in results[repo]])}\n")
+
+
+def analyze_results(file: str, model_name: str) -> None:
+    """
+    Analyze the results of the benchmark run, print precision, recall, and MRR.
+    :param file: name of the file, not the full path.
+    :param model: the model that suggests libraries.
+    :return: None.
+    """
+    def precision_recall_k(repo: str, k: int) -> Tuple[float, float, Set[str]]:
+        """
+        Calculate the precision and recall of first K predictions for a repo.
+        :param repo: full name of the repo.
+        :param k: number of first predictions to consider.
+        :return: tuple (precision, recall, correct guesses)
+        """
+        true_libraries = benchmark[repo]
+        suggestions = results[repo][:k]
+        positive_guesses = set(true_libraries).intersection(set(suggestions))
+        precision = len(positive_guesses) / len(suggestions)
+        recall = len(positive_guesses) / len(true_libraries)
+        return precision, recall, positive_guesses
+
+    def mrr(repo: str) -> float:
+        """
+        Calculate the MRR of the prediction for a repo.
+        :param repo: full name of the repo.
+        :return: float of the MRR.
+        """
+        true_libraries = benchmark[repo]
+        suggestions = results[repo]
+        for index, req in enumerate(suggestions):
+            if req in true_libraries:
+                return 1/(index + 1)
+        return 0
+
+    benchmark = {} # Upload the benchmark.
+    with open(f"benchmark/{file}_benchmark") as fin:
+        for line in fin:
+            data = line.rstrip().split(";")
+            benchmark[data[0]] = data[1].split(",")
+    results = {} # Upload the prediction.
+    with open(f"benchmark/results/{file}-{model_name}-results") as fin:
+        for line in fin:
+            data = line.rstrip().split(";")
+            results[data[0]] = data[1].split(",")
+
+    precision = {}
+    recall = {}
+    mrrs = []
+    for repo in tqdm(results): # Iterate over the predicted repos.
+        for k in [1, 5, 10, 30]: # Iterate over different ranges.
+            p, r, _ = precision_recall_k(repo, k)
+            if k not in precision: # Gather precisions and recalls for each range into lists.
+                precision[k] = []
+            if k not in recall:
+                recall[k] = []
+            precision[k].append(p)
+            recall[k].append(r)
+        mrrs.append(mrr(repo)) # Gather MRRs into a list
+    for k in precision:
+        print(f"First {k}: precision {round(mean(precision[k]), 4)}, " # Print average metrics.
+              f"recall {round(mean(recall[k]), 4)}")
+    print(f"MRR: {round(mean(mrrs), 4)}")
+
+
+if __name__ == "__main__":
+    # create_diffs("requirements_history.txt")
+    # create_benchmark("requirements_history.txt")
+    # baseline("requirements_history.txt")
+    # run_benchmark("requirements_history.txt", suggest_libraries)
+    # analyze_results("requirements_history.txt", "suggest_libraries-0.9")
+    pass
